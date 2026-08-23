d4f6d98d17198c0e2e52e6b9f280a8ed4d73a2a7
DHEERAJ OK <dheerajokfreefire@gmail.com>
2026-08-23 07:48:19 +0530

    added

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
   );
 
   const port = process.env.PORT || 5000;
-  await app.listen(port);
-  console.log(`🚀 EcoVerse Backend Server running on http://localhost:${port}/api`);
+  await app.listen(port, '0.0.0.0');
+  console.log(`🚀 EcoVerse Backend Server running on port ${port}`);
 }
 bootstrap();
